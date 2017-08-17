8fb418c2c4ae48b7b55441bb48f0b80146ef757b
NY7894 <NY7894@STLW10ASNYATS03>
2017-08-17 09:44:08 -0400

    fixed gender value for SK ecert file

---
+++ ecertSeeder.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using eCrtSeederNS;
+
+namespace ecertSeeder.Tests
+{
+    [TestClass]
+    public class SInTest
+    {
+        [TestMethod]
+        public void TestalidSIN()
+        {
+
+            long test= ValidSIN.GenerateValidSIN();
+        }
+    }
+}
