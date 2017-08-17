a301d1d92fdbb97d00cfbcb9acb181f61dab6c6e
NY7894 <NY7894@STLW10ASNYATS03>
2017-08-17 10:49:37 -0400

    added unit test for SIN

---
+++ ecertSeeder.Tests/TestLuhnAlgorithm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using eCrtSeederNS;
+
+namespace ecertSeeder.Tests
+{
+    [TestClass]
+    public class SIN_Test
+    {
+        [TestMethod]
+        public void InValidSIN()
+        {
+
+            string InvalidSIN = "111333444";
+            Assert.IsFalse(InvalidSIN.LuhnCheck());
+        }
+        [TestMethod]
+        public void ValidSIN()
+        {
+
+            string ValidSIN = "430665687";
+            Assert.IsTrue(ValidSIN.LuhnCheck());
+        }
+    }
+}
